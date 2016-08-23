442e8bcd55e3c9ec4c738bcb73638d01827573f9
Raoul Hecky <raoul.hecky@gmail.com>
2016-08-23 11:58:24 +0200

    fix env for travis

--- scripts/ci/linux/install.sh
+++ scripts/ci/linux/install.sh
@@ -5,7 +5,7 @@ tar xJf mxe_qt57.tar.xz
 
 unset `env | \
        grep -vi '^EDITOR=\|^HOME=\|^LANG=\|MXE\|^PATH=' | \
-       grep -vi 'PKG_CONFIG\|PROXY\|^PS1=\|^TERM=' | \
+       grep -vi 'PKG_CONFIG\|PROXY\|^PS1=\|^TERM=\|^TRAVIS_OS_NAME=' | \
        cut -d '=' -f1 | tr '\n' ' '`
 
 export PATH=$(pwd)/mxe/usr/bin:$PATH
