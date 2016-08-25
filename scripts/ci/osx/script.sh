1b86536245a146bdebb8234f6ecef3cf881cf061
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2016-08-25 16:42:45 +0200

    Add first version of OSX package

---
+++ scripts/ci/osx/script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ev
+
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $SCRIPTDIR/funcs.sh
+
+mkdir build
+pushd build
+
+unset `env | \
+grep -vi '^EDITOR=\|^HOME=\|^LANG=\|MXE\|^PATH=' | \
+grep -vi 'PKG_CONFIG\|PROXY\|^PS1=\|^TERM=\|^TRAVIS_OS_NAME=' | \
+   cut -d '=' -f1 | tr '\n' ' '`
+
+QTDIR="/usr/local/opt/qt5"
+PATH="$QTDIR/bin:$PATH"
+LDFLAGS=-L$QTDIR/lib
+CPPFLAGS=-I$QTDIR/include
+
+make_version ..
+
+qmake ../Moolticute.pro
+make
+
+popd
