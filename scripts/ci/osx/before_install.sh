c6c5907248fd2aa7a31cc0c842722b88f9f71439
Raoul Hecky <raoul.hecky@gmail.com>
2016-09-01 10:44:54 +0200

    move openssl command to script

--- scripts/ci/osx/before_install.sh
+++ scripts/ci/osx/before_install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-echo "Nothing to do before install"
+openssl aes-256-cbc \
+    -K $encrypted_7917762619ed_key \
+    -iv $encrypted_7917762619ed_iv \
+    -in scripts/ci/osx/developerID_application.cer.enc \
+    -out scripts/ci/osx/developerID_application.cer \
+    -d
