024da89c0e285f45ebbd40a317c193b642822914
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2016-09-03 12:59:50 +0200

    Simplify dmg creation script by using an external tool appdmg (https://github.com/LinusU/node-appdmg). Add appdmg.json file who describe how to build the dmg Add xcf and png background for the dmg Remove commented code in script

--- scripts/ci/osx/after_success.sh
+++ scripts/ci/osx/after_success.sh
@@ -39,20 +39,6 @@ rm -f build/$APP.app/Contents/Info.plist-e
 # Copy daemon to bundle
 cp build/moolticuted build/$APP.app/Contents/MacOS/
 
-# copy translation files to app
-# cp languages/*.qm build/$APP.app/Contents/Resources
-
-echo "Adding keys"
-# add the keys for OSX code signing
-#security create-keychain -p travis osx-build.keychain
-#security import ../travis/osx/apple.cer -k ~/Library/Keychains/osx-build.keychain -T /usr/bin/codesign
-#security import scripts/ci/osx/developerID_application.cer -k osx-build.keychain -T /usr/bin/codesign
-#security import ../travis/osx/dist.p12 -k ~/Library/Keychains/osx-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
-#security default-keychain -s osx-build.keychain
-#security unlock-keychain -p travis osx-build.keychain
-
-#security find-identity -v
-
 # use macdeployqt to deploy the application
 #echo "Calling macdeployqt and code signing application"
 #$QTDIR/bin/macdeployqt ./$APP.app -codesign="$DEVELOPER_NAME"
@@ -70,86 +56,10 @@ wget https://raw.githubusercontent.com/aurelien-rainone/macdeployqtfix/master/ma
 python macdeployqtfix.py build/$APP.app/Contents/MacOS/MoolticuteApp /usr/local/Cellar/qt5/5.*/
 python macdeployqtfix.py build/$APP.app/Contents/MacOS/moolticuted /usr/local/Cellar/qt5/5.*/
 
-#echo "Sign the code"
-##This signs the code
-echo "Sign Code with $SIGNATURE"
+#install appdmg https://github.com/LinusU/node-appdmg a tool to create awesome dmg !
+npm install -g appdmg
+appdmg mac/appdmg.json build/$APP-$VERSION.dmg
 
-#codesign --force --verbose --sign "Developer ID Application: Raoul Hecky" build/$APP.app/Contents/Frameworks/*
-#find build/$APP.app/Contents/plugins -name "*dylib" --exec codesign codesign --force --verbose --sign "Developer ID Application: $SIGNATURE" {} \;
-#codesign --force --verbose --sign "Developer ID Application: $SIGNATURE" build/$APP.app
-#codesign -s "$SIGNATURE" -f build/$APP.app
-#if [ "$?" -ne "0" ]; then
-#    echo "Failed to sign app bundle"
-#    exit 1
-#fi
-
-
-
-echo "Verifying code signed app"
-#codesign --verify --verbose=4 build/$APP.app
-#spctl --assess --verbose=4 --raw build/$APP.app
-
-echo "Create $TEMPDIR"
-#Create a temporary directory if one doesn't exist
-mkdir -p $TEMPDIR
-if [ "$?" -ne "0" ]; then
-    echo "Failed to create temporary folder"
-    exit 1
-fi
-
-echo "Clean $TEMPDIR"
-#Delete the contents of any previous builds
-rm -Rf ./$TEMPDIR/*
-if [ "$?" -ne "0" ]; then
-    echo "Failed to clean temporary folder"
-    exit 1
-fi
-
-echo "Move application bundle"
-#Move the application to the temporary directory
-mv build/$APP.app ./$TEMPDIR
-if [ "$?" -ne "0" ]; then
-    echo "Failed to move application bundle"
-    exit 1
-fi
-
-echo "Create symbolic link"
-#Create a symbolic link to the applications folder
-ln -s /Applications ./$TEMPDIR/Applications
-if [ "$?" -ne "0" ]; then
-    echo "Failed to create link to /Applications"
-    exit 1
-fi
-
-echo "Create new disk image"
-#Create the disk image
-rm -f build/$APP.dmg
-hdiutil create -srcfolder ./$TEMPDIR -format UDBZ build/$APP.dmg
-if [ "$?" -ne "0" ]; then
-    echo "Failed to create disk image"
-    exit 1
-fi
-
-#echo "Code signing disk image"
-#codesign --force --verify --verbose --sign "$DEVELOPER_NAME" build/$APP.dmg
-
-echo "Verifying code signed disk image"
-#codesign --verify --verbose=4 build/$APP.dmg
-#spctl --assess --verbose=4 --raw build/$APP.dmg
-
-echo "moving $APP.dmg to $APP-$VERSION.dmg"
-mv build/$APP.dmg build/$APP-$VERSION.dmg
-
-echo "Removing keys"
-# remove keys
-#security delete-keychain osx-build.keychain 
-
-# delete the temporary directory
-rm -Rf ./$TEMPDIR/*
-if [ "$?" -ne "0" ]; then
-    echo "Failed to clean temporary folder"
-    exit 1
-fi
 
 upload_file build/$APP-$VERSION.dmg $(shasum -a 256 build/$APP-$VERSION.dmg | cut -d' ' -f1) "macos"
 
