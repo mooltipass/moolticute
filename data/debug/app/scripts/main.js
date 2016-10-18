dc66bfae6f9494e8557a5a42d3f105fcd2e9ef1f
Raoul Hecky <raoul.hecky@gmail.com>
2016-10-18 10:01:28 +0200

    implement get_data_node api to retrieve data node from device

--- data/debug/app/scripts/main.js
+++ data/debug/app/scripts/main.js
@@ -5,10 +5,11 @@ var apiList = ['{"msg": ""}',
                '{"msg": "param_set", "data": {"flash_screen": false, "keyboard_layout": 155,"knock_enabled": false, "knock_sensitivity": 1,"lock_timeout": 0,"lock_timeout_enabled": false,"offline_mode": false,"screen_brightness": 51, "screensaver": false,"tutorial_enabled": false,"user_interaction_timeout": 0,"user_request_cancel": false}}',
                '{ "msg": "start_memorymgmt" }',
                '{ "msg": "exit_memorymgmt" }',
-               '{"msg": "ask_password", "data": {"service": "sub.example.com", "fallback_service": "example.com", "login": "user@example.com", "request_id": "123"}}',
+               '{"msg": "get_credential", "data": {"service": "sub.example.com", "fallback_service": "example.com", "login": "user@example.com", "request_id": "123"}}',
                '{"msg": "set_credential", "data": {"service": "example.com", "login": "user@example.com", "password": "pwd", "description": "added by API"}}',
                '{"msg": "get_random_numbers"}',
-               '{"msg": "cancel_request", "data": {"request_id": "123"}}'
+               '{"msg": "cancel_request", "data": {"request_id": "123"}}',
+               '{"msg": "get_data_node", "data": {"service": "data example", "fallback_service": "data example2", "request_id": "123"}}'
 ]
 
 function popuplateApiList() {
