b33baf2dc289b0bb449dc82cae7da9a5d9793f35
Corentin Jabot <corentinjabot@gmail.com>
2017-02-24 00:03:44 +0100

    Add a random password generation widget.
     * Supports configuring the length of a password
     * supports uper case, lower case, digits & symbols
     * generation based on mersen twister & std::random device

--- src/PasswordLineEdit.h
+++ src/PasswordLineEdit.h
@@ -3,12 +3,47 @@
 
 #include <QLineEdit>
 
+class QPushButton;
+class QSlider;
+class QCheckBox;
+class QLabel;
 
+class PasswordOptionsPopup : public QFrame {
+    Q_OBJECT
+
+public:
+    PasswordOptionsPopup(QWidget* parent);
+Q_SIGNALS:
+    void passwordGenerated(const QString & password);
+
+private Q_SLOTS:
+    void generatePassword();
+    void updatePasswordLength(int);
+    void emitPassword();
+
+protected:
+    void showEvent(QShowEvent* e) override;
+
+private:
+    QPushButton* m_refreshBtn, *m_fillBtn;
+    QSlider* m_lengthSlider;
+    QCheckBox *m_upperCaseCB, *m_lowerCaseCB, *m_digitsCB, *m_symbolsCB;
+    QLabel *m_passwordLabel, * m_sliderLengthLabel;
+
+    std::mt19937 m_random_generator;
+};
+
+
+class PasswordOptionsPopup;
 class PasswordLineEdit : public QLineEdit
 {
 public:
     PasswordLineEdit(QWidget* parent = nullptr);
-    QAction *m_showPassword, *m_hidePassword;
+    QAction *m_showPassword, *m_hidePassword, *m_generateRandom;
+    PasswordOptionsPopup* m_passwordOptionsPopup;
+
+private:
+    void showPasswordOptions();
 };
 
 #endif // PASSWORDLINEEDIT_H
