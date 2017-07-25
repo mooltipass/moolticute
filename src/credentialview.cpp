4f7b0019c6aab4d2b4b9cb31d12e98067bc14be0
flegall2014 <legallfranck@yahoo.com>
2017-07-25 08:37:19 +0200

    Added setItemDelegate back

--- src/credentialview.cpp
+++ src/credentialview.cpp
@@ -59,7 +59,7 @@ CredentialView::CredentialView(QWidget *parent) : QTreeView(parent),
     m_bIsFullyExpanded(false)
 {
     setHeaderHidden(true);
-    //setItemDelegateForColumn(0, new ItemDelegate(this));
+    setItemDelegateForColumn(0, new ItemDelegate(this));
     setMinimumWidth(430);
     connect(this, &CredentialView::clicked, this, &CredentialView::onExpandItem);
 }
