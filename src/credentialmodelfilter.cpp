f078fd5e37c522ceafa82b15b058b45d5da64882
flegall2014 <legallfranck@yahoo.com>
2017-07-29 21:44:30 +0200

    Fix item ordering when adding a new login

--- src/credentialmodelfilter.cpp
+++ src/credentialmodelfilter.cpp
@@ -6,7 +6,7 @@
 
 CredentialModelFilter::CredentialModelFilter(QObject *parent) : QSortFilterProxyModel(parent)
 {
-    setDynamicSortFilter(false);
+    setDynamicSortFilter(true);
     sort(0);
 }
 
