9f307c975db23d51ddbe7bb5cd1c346f626de9d8
Raoul Hecky <raoul.hecky@gmail.com>
2017-09-06 09:02:51 +0000

    Fix build

--- scripts/ci/linux/before_install.sh
+++ scripts/ci/linux/before_install.sh
@@ -8,7 +8,7 @@ sudo add-apt-repository -y ppa:ubuntu-wine/ppa
 sudo dpkg --add-architecture i386
 sudo apt-get update -qq
 sudo apt-get -y install --install-recommends wine1.7
-wget_retry https://calaos.fr/download/misc/InnoSetup4.zip -O $HOME/InnoSetup5.zip
+wget_retry https://calaos.fr/download/misc/InnoSetup5.zip -O $HOME/InnoSetup5.zip
 
 mkdir -p "$HOME/.wine/drive_c/Program Files (x86)/"
 pushd "$HOME/.wine/drive_c/Program Files (x86)/"
