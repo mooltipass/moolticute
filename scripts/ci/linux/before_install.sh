85db1d86a2d16f02d7571537265a24dcb0914545
Raoul Hecky <raoul.hecky@gmail.com>
2017-10-23 12:05:11 +0000

    Use our own version of osslsigncode

--- scripts/ci/linux/before_install.sh
+++ scripts/ci/linux/before_install.sh
@@ -5,11 +5,15 @@ SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $SCRIPTDIR/../funcs.sh
 
 sudo add-apt-repository -y ppa:ubuntu-wine/ppa
-sudo add-apt-repository -y ppa:likemartinma/osslsigncode
 sudo dpkg --add-architecture i386
 sudo apt-get update -qq
-sudo apt-get -y install --install-recommends wine1.7 osslsigncode
+sudo apt-get -y install --install-recommends wine1.7
 wget_retry https://calaos.fr/download/misc/InnoSetup5.zip -O $HOME/InnoSetup5.zip
+
+#download our signcode tool
+wget_retry https://github.com/mooltipass/osslsigncode/releases/download/1.7.1/osslsigncode_amd64.tar.xz -O $HOME/osslsigncode_amd64.tar.xz
+tar xJvf $HOME/osslsigncode_amd64.tar.xz
+sudo mv osslsigncode /usr/bin/local
 
 mkdir -p "$HOME/.wine/drive_c/Program Files (x86)/"
 pushd "$HOME/.wine/drive_c/Program Files (x86)/"
