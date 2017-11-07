453b3dd21a78757cb62078ea2e0d683fd19ce9fc
Alexis López Zubieta <azubieta90@gmail.com>
2017-11-06 18:19:43 -0600

    set nano inside docker

--- scripts/ci/linux/after_success.sh
+++ scripts/ci/linux/after_success.sh
@@ -70,7 +70,7 @@ echo "Generating changelog for tag ${BUILD_TAG} [${TRAVIS_COMMIT}]"
 rm -f debian/changelog
 
 $DOCKER_EXEC 'echo "deb http://mx.archive.ubuntu.com/ubuntu/ xenial main restricted" > /etc/apt/sources.list'
-$DOCKER_EXEC "apt-get update; apt-get install vi"
+$DOCKER_EXEC "apt-get update; apt-get install nano"
 $DOCKER_EXEC "DEBEMAIL=${USER_EMAIL} dch --create --distribution trusty --package \"moolticute\" \
     --newversion ${DEB_VERSION} \"Release ${BUILD_TAG}\""
 
