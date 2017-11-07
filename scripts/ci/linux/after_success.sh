b3ca2211fc570e0b406072adf165e8732c756f17
Alexis López Zubieta <azubieta90@gmail.com>
2017-11-06 18:32:12 -0600

    attempting with /bin/true

--- scripts/ci/linux/after_success.sh
+++ scripts/ci/linux/after_success.sh
@@ -69,9 +69,7 @@ echo "Generating changelog for tag ${BUILD_TAG} [${TRAVIS_COMMIT}]"
 
 rm -f debian/changelog
 
-$DOCKER_EXEC 'echo "deb http://mx.archive.ubuntu.com/ubuntu/ xenial main restricted" > /etc/apt/sources.list'
-$DOCKER_EXEC "apt-get update; apt-get install nano"
-$DOCKER_EXEC "DEBEMAIL=${USER_EMAIL} dch --create --distribution trusty --package \"moolticute\" \
+$DOCKER_EXEC "VISUAL='/bin/true' DEBEMAIL=${USER_EMAIL} dch --create --distribution trusty --package \"moolticute\" \
     --newversion ${DEB_VERSION} \"Release ${BUILD_TAG}\""
 
 echo "Building .deb package..."
