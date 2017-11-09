9ab8e8a4900e329cb748a2470b36d87995e97877
Alexis López Zubieta <azubieta90@gmail.com>
2017-11-09 12:40:26 -0600

    Check udev rules

--- data/moolticute.sh
+++ data/moolticute.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-
+##
+# Wrapper for the moolticute AppImage
+##
 me="Moolticute.AppImage"
+UDEV_RULES_FILE_PATH="/etc/udev/rules.d/50-mooltipass.rules"
+
 help_msg="Moolticute for linux
 Usage:
 $me <arg>
 
 Arguments:
--d --daemon        Runs daemon only mode.
--i --install       Install moolticute as a systemd service
--u --uninstall     Uninstall moolticute as a systemd service
--h --help          This help
+-d --daemon         Runs daemon only mode.
+-i --install        Install moolticute UDev rules
+-c --check          Check moolticute UDev rules
+-u --uninstall      Uninstall moolticute UDev rules
+-h --help           This help
 
 Example:
 Run Moolticute normaly
@@ -22,6 +27,7 @@ Run only the moolticuted daemon
 DAEMON_ONLY=0
 INSTALL=0
 UNINSTALL=0
+CHECK=0
 
 
 case "$1" in
@@ -31,6 +37,10 @@ case "$1" in
         ;;
     -i|--install)
         INSTALL=1
+        shift
+    ;;
+        -c|--check)
+        CHECK=1
         shift
     ;;
     -u|--uninstall)
@@ -52,13 +62,23 @@ if (( $DAEMON_ONLY == 1 )) ;
 then
     echo "Running daemon only"
     moolticuted
-elif (( $INSTALL == 0 )) && (( $UNINSTALL == 0 ));
+elif (( $INSTALL == 0 )) && (( $UNINSTALL == 0 )) && (( $CHECK == 0 ));
 then
     moolticute
 elif (( $INSTALL == 1 ));
 then
-    echo "Installing moolticuted as service"
+    echo "Installing moolticute UDEV rules"
+elif (( $CHECK == 1 ));
+then
+    if [ -s $UDEV_RULES_FILE_PATH ];
+    then
+        echo "Moolticute Udev rules are NOT installed."
+        exit 1;
+    else
+        echo "Moolticute Udev rules are installed."
+        exit 0;
+    fi
 elif (( $UNINSTALL == 1 ));
 then
-    echo "Uninstalling moolticuted as service"
+    echo "Uninstalling moolticuted UDEV rules"
 fi
