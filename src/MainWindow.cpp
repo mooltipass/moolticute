6c3a4a59a25a94547d76a820bcd50f994bc6b89c
Alexander Sashnov <sashnov@ngs.ru>
2018-05-15 16:43:38 +0700

    Issue #209: Add (i) label with a tooltip about resetting a card.

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -61,7 +61,8 @@ void MainWindow::initHelpLabels()
     ui->label_MooltiAppHelp->setPixmap(getFontAwesomeIconPixmap(fa::infocircle));
     ui->label_MooltiAppHelp->setToolTip(tr("The MooltiApp backup file doesn't have encrypted logins."));
 
-
+    ui->label_resetCardHelp->setPixmap(getFontAwesomeIconPixmap(fa::infocircle));
+    ui->label_resetCardHelp->setToolTip(tr("When an unknown card message is displayed that means you have no database for this user in your Mooltipass device.\nHovewer you or other users may have a backup file or may use this card in another device.\nThink twice before resetting a card."));
 }
 
 MainWindow::MainWindow(WSClient *client, QWidget *parent) :
