2853d6269da15bc666a861dd71d4c49011084464
Morten Kristensen <msk@nullpointer.dk>
2018-06-12 21:14:04 +0200

    Disable tests on Windows

--- scripts/ci/linux/script.sh
+++ scripts/ci/linux/script.sh
@@ -24,8 +24,6 @@ export PATH=$HOME/mxe/usr/bin:$PATH
 export MXE_BASE=$HOME/mxe
 
 $MXE_BASE/usr/i686-w64-mingw32.shared.posix/qt5/bin/qmake ../Moolticute.pro
-
-# Compund exit codes of make and the tests.
-make && tests/release/tests.exe
+make
 
 popd
