79d7587f5da9ab75ec92a90c43a89f685e501140
Raoul Hecky <raoul.hecky@gmail.com>
2018-09-12 12:23:20 +0200

    fix gpg keys

--- scripts/ci/linux/before_install.sh
+++ scripts/ci/linux/before_install.sh
@@ -13,11 +13,9 @@ docker pull mooltipass/mc-upload
 #create certificate from env
 echo $CODESIGN_WIN_CERT | base64 -d > $HOME/cert.p12
 
-VERSION=$(get_version)
-
 #setup the source dir for ubuntu deb
 rm -fr $HOME/build-debs && mkdir -p $HOME/build-debs
-echo $LAUNCHPAD_GPG_PUB | base64 -d > $HOME/build-debs/gpgkey_pub.asc
-echo $LAUNCHPAD_GPG_PRIV | base64 -d > $HOME/build-debs/gpgkey_sec.asc
-echo $LAUNCHPAD_GPG_PASS | base64 -d > $HOME/build-debs/passphrase.txt
+echo -e $LAUNCHPAD_GPG_PUB > $HOME/build-debs/gpgkey_pub.asc
+echo -e $LAUNCHPAD_GPG_PRIV > $HOME/build-debs/gpgkey_sec.asc
+echo $LAUNCHPAD_GPG_PASS > $HOME/build-debs/passphrase.txt
 
