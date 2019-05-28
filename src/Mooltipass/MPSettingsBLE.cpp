2c538d6d066c7ecf63f18a517672cfc518fabc61
Oliver Toth <totholiver92@gmail.com>
2019-05-28 20:52:20 +0200

    Add BLE support for already implemented device settings on daemon side

--- src/Mooltipass/MPSettingsBLE.cpp
+++ src/Mooltipass/MPSettingsBLE.cpp
@@ -25,7 +25,10 @@ void MPSettingsBLE::loadParameters()
                         m_settings = pMesProt->getFullPayload(data);
                         qDebug() << "Full device settings payload: " << m_settings.toHex();
                         qWarning() << "Load Parameters processing haven't been implemented for BLE yet.";
-                        set_randomStartingPin(m_settings.at(RANDOM_PIN_BYTE_ID) != 0);
+                        set_reservedBle(m_settings.at(RESERVED_BYTE) != 0);
+                        set_userInteractionTimeout(m_settings.at(USER_INTERACTION_TIMEOUT_BYTE));
+                        set_randomStartingPin(m_settings.at(RANDOM_PIN_BYTE) != 0);
+                        set_promptAnimation(m_settings.at(ANIMATION_DURING_PROMPT_BYTE) != 0);
                         return true;
                     }
     ));
@@ -35,11 +38,7 @@ void MPSettingsBLE::loadParameters()
 
 void MPSettingsBLE::updateParam(MPParams::Param param, int val)
 {
-    //TODO: Updating params, when implemented on fw side
-    if (param == MPParams::RANDOM_INIT_PIN_PARAM)
-    {
-        m_settings[RANDOM_PIN_BYTE_ID] = static_cast<char>(val);
-    }
+    m_settings[m_bleByteMapping[param]] = static_cast<char>(val);
 }
 
 void MPSettingsBLE::connectSendParams(WSServerCon *wsServerCon)
@@ -76,5 +75,10 @@ void MPSettingsBLE::setSettings()
 
 void MPSettingsBLE::fillParameterMapping()
 {
+    m_bleByteMapping[MPParams::RANDOM_INIT_PIN_PARAM] = RANDOM_PIN_BYTE;
+    m_bleByteMapping[MPParams::USER_INTER_TIMEOUT_PARAM] = USER_INTERACTION_TIMEOUT_BYTE;
+    m_paramMap.insert(MPParams::RESERVED_BLE, "reserved_ble");
+    m_bleByteMapping[MPParams::RESERVED_BLE] = RESERVED_BYTE;
     m_paramMap.insert(MPParams::PROMPT_ANIMATION_PARAM, "prompt_animation");
+    m_bleByteMapping[MPParams::PROMPT_ANIMATION_PARAM] = ANIMATION_DURING_PROMPT_BYTE;
 }
--- src/Mooltipass/MPSettingsBLE.h
+++ src/Mooltipass/MPSettingsBLE.h
@@ -8,6 +8,7 @@ class MPSettingsBLE : public MPSettings
     Q_OBJECT
 
     //MP BLE only
+    QT_SETTINGS_PROPERTY(bool, reservedBle, false, MPParams::RESERVED_BLE)
     QT_SETTINGS_PROPERTY(bool, promptAnimation, false, MPParams::PROMPT_ANIMATION_PARAM)
 
 public:
@@ -24,8 +25,12 @@ private:
     void fillParameterMapping();
 
     QByteArray m_settings;
+    QMap<MPParams::Param, int> m_bleByteMapping;
 
-    static constexpr int RANDOM_PIN_BYTE_ID = 5;
+    static constexpr int RESERVED_BYTE = 0;
+    static constexpr int RANDOM_PIN_BYTE = 1;
+    static constexpr int USER_INTERACTION_TIMEOUT_BYTE = 2;
+    static constexpr int ANIMATION_DURING_PROMPT_BYTE = 3;
 };
 
 #endif // MPSETTINGSBLE_H
