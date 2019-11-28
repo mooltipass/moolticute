730a1d99e0537dc0079ec81033cc6419d7704d85
Raoul Hecky <raoul.hecky@gmail.com>
2019-11-28 15:46:40 +0100

    add ubuntu eoan build

--- scripts/ci/linux/after_success.sh
+++ scripts/ci/linux/after_success.sh
@@ -16,6 +16,7 @@ fi
 docker exec mc-deb bash /scripts/build_source.sh $VERSION xenial
 docker exec mc-deb bash /scripts/build_source.sh $VERSION bionic
 docker exec mc-deb bash /scripts/build_source.sh $VERSION disco
+docker exec mc-deb bash /scripts/build_source.sh $VERSION eoan
 
 #windows and appimage
 docker exec appimgbuilder bash /scripts/package.sh
