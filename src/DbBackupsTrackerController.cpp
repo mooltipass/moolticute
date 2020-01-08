9ff46fc73f90a9c5d233da5f1abfbfcd31f07215
Oliver Toth <totholiver92@gmail.com>
2020-01-08 20:55:57 +0100

    [BLE] Enable Database Backup Monitoring

--- src/DbBackupsTrackerController.cpp
+++ src/DbBackupsTrackerController.cpp
@@ -246,7 +246,7 @@ void DbBackupsTrackerController::handleDeviceConnectedChanged(const bool &)
 
 void DbBackupsTrackerController::handleFirmwareVersionChange(const QString &)
 {
-    if (wsClient->isFw12())
+    if (wsClient->isFw12() || wsClient->isMPBLE())
     {
         qDebug() << "Setup db tracking";
         window->showDbBackTrackingControls(true);
