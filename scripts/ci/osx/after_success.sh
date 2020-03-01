3775770a178003d6764deb7211db72c3623109b0
Mathieu <limpkin@limpkin.fr>
2020-03-01 20:24:15 +0100

    Update after_success.sh

--- scripts/ci/osx/after_success.sh
+++ scripts/ci/osx/after_success.sh
@@ -12,7 +12,7 @@ source $SCRIPTDIR/../funcs.sh
 VERSION="$(get_version .)"
 
 #Only build if the commit we are building is for the last tag
-if [ "$(git rev-list -n 1 $VERSION)" != "$(cat .git/HEAD)"  ]; then
+if [ "$(git rev-list -n 1 $VERSION)" != "$(git rev-parse HEAD)"  ]; then
     echo "Not uploading package"
     exit 0
 fi
