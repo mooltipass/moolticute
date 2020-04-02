23edb9e33b5211257ad5cd91ae8b51fcf3671c4e
Oliver Toth <dexol12@gmail.com>
2020-04-02 12:52:05 +0200

    [BLE] Only process security settings, when device is unlocked

--- src/MPDevice.cpp
+++ src/MPDevice.cpp
@@ -3528,7 +3528,7 @@ void MPDevice::processStatusChange(const QByteArray &data)
     Common::MPStatus s = pMesProt->getStatus(data);
     Common::MPStatus prevStatus = get_status();
 
-    if (isBLE() && pMesProt->getMessageSize(data) >= 4)
+    if (isBLE() && Common::Unlocked == s)
     {
         bleImpl->readUserSettings(pMesProt->getPayloadBytes(data, 2, 4));
     }
