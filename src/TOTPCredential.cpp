1a3db5bedcfe13469ed95ca4519085f2019b8e40
Oliver Toth <totholiver92@gmail.com>
2020-10-23 20:44:10 +0200

    [BLE] Display error if invalid base32 char is entered

--- src/TOTPCredential.cpp
+++ src/TOTPCredential.cpp
@@ -1,12 +1,18 @@
 #include "TOTPCredential.h"
 #include "ui_TOTPCredential.h"
 
+#include <QPushButton>
+
+const QString TOTPCredential::BASE32_REGEXP = "[^A-Z2-7=*]";
+
 TOTPCredential::TOTPCredential(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::TOTPCredential)
 {
     ui->setupUi(this);
     setWindowFlags(Qt::Dialog | Qt::MSWindowsFixedSizeDialogHint);
+    ui->labelError->setStyleSheet("QLabel { color : red; }");
+    ui->labelError->hide();
 }
 
 TOTPCredential::~TOTPCredential()
@@ -34,4 +40,18 @@ void TOTPCredential::clearFields()
     ui->lineEditSecretKey->clear();
     ui->spinBoxTimeStep->setValue(DEFAULT_TIME_STEP);
     ui->spinBoxCodeSize->setValue(DEFAULT_CODE_SIZE);
+}
+
+void TOTPCredential::on_lineEditSecretKey_textChanged(const QString &arg1)
+{
+    if (arg1.contains(QRegExp(BASE32_REGEXP)))
+    {
+        ui->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
+        ui->labelError->show();
+    }
+    else
+    {
+        ui->buttonBox->button(QDialogButtonBox::Ok)->setEnabled(true);
+        ui->labelError->hide();
+    }
 }
--- src/TOTPCredential.h
+++ src/TOTPCredential.h
@@ -21,11 +21,15 @@ public:
 public slots:
     void clearFields();
 
+private slots:
+    void on_lineEditSecretKey_textChanged(const QString &arg1);
+
 private:
     Ui::TOTPCredential *ui;
 
     static constexpr int DEFAULT_TIME_STEP = 30;
     static constexpr int DEFAULT_CODE_SIZE = 6;
+    static const QString BASE32_REGEXP;
 };
 
 #endif // TOTPCREDENTIAL_H
