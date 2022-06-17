7861b1be61105d794c9698fed1d63d9f1e7695db
pokamest <pokamest@gmail.com>
2022-06-17 03:03:35 +0300

    Travis build fix

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -3,7 +3,7 @@ set -ev
 
 echo "install.sh"
 
-brew install jq lftp python3
+brew install jq lftp
 pip3 install aqtinstall
 aqt install-qt mac desktop 6.2.4 clang_64 -m all -O /opt/Qt
 networksetup -setv6off Ethernet
