829ca7c5ecefd2899c86fd07f82df973c5288217
Mathieu <limpkin@limpkin.fr>
2022-10-15 10:42:17 +0200

    Update after_success.sh
    impish is obsolete and will not accept new uploads.

--- scripts/ci/linux/after_success.sh
+++ scripts/ci/linux/after_success.sh
@@ -17,7 +17,6 @@ docker exec mc-deb bash /scripts/build_source.sh $VERSION xenial
 docker exec mc-deb bash /scripts/build_source.sh $VERSION bionic
 docker exec mc-deb bash /scripts/build_source.sh $VERSION focal
 docker exec mc-deb bash /scripts/build_source.sh $VERSION jammy
-docker exec mc-deb bash /scripts/build_source.sh $VERSION impish
 
 #windows and appimage
 docker exec appimgbuilder bash /scripts/package.sh
