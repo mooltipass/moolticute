72dd32368424f4fb5760f83f90c0b433eb7c8250
Mathieu <limpkin@limpkin.fr>
2023-10-26 14:25:00 +0200

    trying to fix macOS ci build

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -4,6 +4,8 @@ set -ev
 echo "install.sh"
 
 brew install jq lftp
+brew uninstall wget
+brew install wget
 pip3 install aqtinstall
 aqt install-qt mac desktop 6.2.4 clang_64 -m all -O /Users/travis/Qt
 networksetup -setv6off Ethernet
