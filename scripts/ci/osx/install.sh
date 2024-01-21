cd22dffbed85c3f8a0b3548b90b12566cd52cffb
Mathieu <limpkin@limpkin.fr>
2024-01-21 23:32:37 +0100

    upgrade pip during travis build

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -6,6 +6,7 @@ echo "install.sh"
 brew install jq lftp
 brew uninstall wget
 brew install wget
+pip3 install --upgrade pip
 pip3 install aqtinstall
 aqt install-qt mac desktop 6.2.4 clang_64 -m all -O /Users/travis/Qt
 networksetup -setv6off Ethernet
