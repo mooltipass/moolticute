8d5b6f77da998361610bfb15200a5ba0e822bc2a
Mathieu <limpkin@limpkin.fr>
2024-01-22 08:49:56 +0100

    adding python to path in ci build

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -6,7 +6,6 @@ echo "install.sh"
 brew install jq lftp
 brew uninstall wget
 brew install wget
-pip3 install --upgrade pip
 
 # Find and export Python binary path
 PY_BIN_PATH=$(which python) || $(which python3)
@@ -14,6 +13,7 @@ echo "Python binary path: $PY_BIN_PATH"
 export PATH="$PY_BIN_PATH:$PATH"
 echo "Updated PATH: $PATH"
 
+pip3 install --upgrade pip
 pip3 install aqtinstall
 aqt install-qt mac desktop 6.2.4 clang_64 -m all -O /Users/travis/Qt
 networksetup -setv6off Ethernet
