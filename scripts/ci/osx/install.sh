d749458fb3197c4a8bc6d901b1c0c3120b7318af
Mathieu <limpkin@limpkin.fr>
2024-01-22 10:39:06 +0100

    specifc aqt install into path

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -8,6 +8,6 @@ brew uninstall wget
 brew install wget
 echo "Current PATH: $PATH"
 pip3 install --upgrade pip
-pip3 install aqtinstall
+pip3 install aqtinstall -t /usr/local/bin
 aqt install-qt mac desktop 6.2.4 clang_64 -m all -O /Users/travis/Qt
 networksetup -setv6off Ethernet
