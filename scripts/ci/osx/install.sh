92209efd5c6ccd77062e1c68b1f62c70aa6605ad
Mathieu <limpkin@limpkin.fr>
2024-01-22 11:47:27 +0100

    trying to fix macos ci build

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -8,6 +8,6 @@ brew uninstall wget
 brew install wget
 echo "Current PATH: $PATH"
 pip3 install --upgrade pip
-pip3 install aqtinstall -t /usr/local/bin
+pip3 install aqtinstall -t /usr/local
 aqt install-qt mac desktop 6.2.4 clang_64 -m all -O /Users/travis/Qt
 networksetup -setv6off Ethernet
