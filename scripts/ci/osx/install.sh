7d635e50d39c81159ef496a1c1e344b807442096
Mathieu <limpkin@limpkin.fr>
2024-01-22 18:48:43 +0100

    adding aqt path to python path for CI

--- scripts/ci/osx/install.sh
+++ scripts/ci/osx/install.sh
@@ -9,5 +9,6 @@ brew install wget
 echo "Current PATH: $PATH"
 pip3 install --upgrade pip
 pip3 install aqtinstall -t /Users/travis/aqt
+export PYTHONPATH=$PYTHONPATH:/Users/travis/aqt
 /Users/travis/aqt/bin/aqt install-qt mac desktop 6.2.4 clang_64 -m all -O /Users/travis/Qt
 networksetup -setv6off Ethernet
