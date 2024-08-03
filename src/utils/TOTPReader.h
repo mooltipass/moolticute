5afb2110d566621e36e34ff33272e3e66809b197
Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>
2024-08-03 14:02:53 +0300

    utils: Use QUrl/QUrlQuery to parse urls
    Apart from not 'reinventing the wheel', this fixes urldecoding in
    logins e.g. cloudflare
    
    Signed-off-by: Andrew 'Necromant' Andrianov <andrew@ncrmnt.org>

--- src/utils/TOTPReader.h
+++ src/utils/TOTPReader.h
@@ -34,15 +34,13 @@ public:
 private:
     static void setupDecoder();
     static TOTPResult processDecodedQR(const QString& res);
-    static QString getParam(const QString& params, const QString& selectedParam);
 
     static const int DEFAULT_DIGITS = 6;
     static const int DEFAULT_PERIOD = 30;
     static QZXing m_decoder;
     static bool m_qr_decoder_set;
-    static const QString TOTP_URI_START;
-    static const QString PARAMS_START;
-    static const QString PARAMS_SEPARATOR;
+    static const QString TOTP_URI;
+    static const QString TOTP_HOST;
     static const QString SECRET;
     static const QString DIGITS;
     static const QString PERIOD;
